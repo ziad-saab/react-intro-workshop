7a4a90d80edd92d3ec4b4cdb6b10400a7a268669
TSirrus <tsirrus@gmail.com>
2017-08-01 00:38:22 +0000

    Done NumberGuessing

---
+++ src/GithubProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class GithubProfile extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        
+    }
+}
+
+export default GithubProfile;
--- src/NumberGuessing.js
+++ src/NumberGuessing.js
@@ -21,6 +21,7 @@ class NumberGuessing extends React.Component {
     }
     
     _handleButtonClick(event) {
+        
         var number = +this.refs.userGuess.value;
         var currentGuesses = this.state.numberGuessed;
         currentGuesses.push(number);
@@ -45,21 +46,35 @@ class NumberGuessing extends React.Component {
                 message: 'You guessed too low!'
             });
         }
+        
+        if (this.state.tries === 1) {
+            this.setState({
+                gameStatus: 'lose'
+            });
+        }
     }
     
     render() {
         if (!this.state.gameStatus) {
             return null;
         }
-        else {
+        else if (this.state.gameStatus === 'playing') {
             return(
                 <div>
                     <p>Guess a number between 1 and 100</p>
                     <p>Your guesses {this.state.numberGuessed.toString()}</p>
-                    <p>{this.state.message} number is {this.state.numberToGuess}</p>
+                    <p>{this.state.message}</p>
                     <p>Guesses left: {this.state.tries}</p>
                     <input type="text" ref="userGuess" />
                     <button type='button' onClick={(event) => {this._handleButtonClick(event)}}>Guess!</button>
+                </div>
+            );
+        }
+        else {
+            return(
+                <div>
+                    <p>YOU {this.state.gameStatus === 'win'? "WIN!" : "LOSE!"}</p>
+                    <button type='button' onClick={(event) => {this._startGame(event)}}>New Game</button>
                 </div>
             );
         }
